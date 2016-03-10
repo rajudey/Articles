63fcc8eac3dcc4f434c3ddb2d8a07bd335ed0909
Raju Dey <raju_grasim@yahoo.com>
2016-03-10 12:05:40 +0000

    create new template and action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,17 +1,16 @@
 class ArticlesController < ApplicationController
   def new
-      @article = Article.new
+    @article = Article.new
   end
   
   def create
-    render plain: params[:article].inspect
+    #render plain: params[:article].inspect
     @article = Article.new(article_params)
     @article.save
-    redirect_to_articles_show(@article)
   end
   
   private
   def article_params
-    params.require(:article).permit(:title, :description, :qty, :price)
+    params.require(:article).permit(:title, :description, :price, :qty)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,13 @@
 Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
+  
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
   root 'page#home'
   get 'about', to: 'page#about'
   
   resources :articles
-  
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
