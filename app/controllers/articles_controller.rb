c8820ae85ba7885ce4a5d116355d5a6a248a5192
Raju Dey <raju_grasim@yahoo.com>
2016-03-10 16:09:36 +0000

    partials+destroy

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,8 @@
 class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all 
+  end
+  
   def new
     @article = Article.new
   end
@@ -14,8 +18,29 @@ class ArticlesController < ApplicationController
     end    
   end
   
+  def edit 
+    @article = Article.find(params[:id])
+  end
+  
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Article was successfully update"
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end     
+  end
+  
   def show
     @article = Article.find(params[:id])
+  end
+  
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    flash[:notice] = "Article is successfully deleted"
+    redirect_to articles_path
   end
   
   private
